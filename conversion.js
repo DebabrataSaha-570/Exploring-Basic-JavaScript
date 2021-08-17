bc1ca0c21586dca53dde87f9133e073500f1d40b
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-18 00:41:13 +0600

    explored javascript convension ,parseint, parsefloat

---
+++ conversion.js
@@ -0,0 +1,42 @@
+var firstName = 'Shuvo'
+var lastName = 'Saha'
+
+// var fullName = firstName + lastName;
+var fullName = firstName + ' ' + lastName
+// console.log(fullName)
+//..........................
+
+
+var first = 'Ariana'
+var last = 'Grande'
+
+var name = first + ' ' + last;
+// console.log(name)
+
+//...............................
+
+var onionPrice = '42';
+var eggPrice = 41;
+
+var totalPrice = onionPrice + eggPrice
+// console.log(totalPrice)
+
+//..............................
+
+//(integer Number) 
+
+var sunGlass = 3;
+
+// float number 
+var price = 99.34
+
+var onionPrice = '42.5';
+var eggPrice = '41.33';
+var onionPriceNumber = parseInt(onionPrice)
+var eggPriceNumber = parseFloat(eggPrice)
+
+console.log(eggPriceNumber)
+console.log(onionPriceNumber)
+
+
+
