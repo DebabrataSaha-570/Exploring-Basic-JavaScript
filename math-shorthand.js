09c8ca81e0de91e01015130c3a309d08ce201ced
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-18 00:16:30 +0600

    explored math shorthand

---
+++ math-shorthand.js
@@ -0,0 +1,67 @@
+var price1 = 37;
+
+// price1 = price1 - 1;
+// price1 -= 1; //(shortcut)
+price1--   //(ultra pro shortcut)
+// console.log(price1)
+
+
+var price2 = 25;
+// price2 = price2 + 1;
+// price2 += 1;     //(shorthand)
+price2++; //(mini shorthand) 
+
+console.log(price2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var price1 = 31;
+// var price2 = 10;
+
+
+// var doublePrice = price2 * 20;
+// // var newPrice = price1 + 10;
+// //  price1 = price1 + 10;
+
+// price1 += 44 //(shorthand)
+
+// // price2 = price2 - 2;
+// price2 -= 2 //(shorthand)
+
+// // console.log(newPrice)
+// // console.log(price1)
+// console.log(price2)
+
+// var age = 14;
+// // age = age + 1;
+// // age += 1; //(shorthand)
+// age++; //(mini shorthand)
+// console.log(age)
+
+
+// var love = 100;
+// // love = love -1 ;
+// // love -= 1; // shorthand
+// love--
+// console.log(love)
